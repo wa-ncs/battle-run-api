d8d6b6f5583883936e5b2e850527567ce0a20180
sbim <sbim@sbim-macbook.local>
2022-07-25 23:36:40 +0900

    create project

---
+++ src/main/java/com/wancs/howmuchspend/HowMuchSpendApplication.java
@@ -0,0 +1,13 @@
+package com.wancs.howmuchspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowMuchSpendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowMuchSpendApplication.class, args);
+	}
+
+}
