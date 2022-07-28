2e98cb88922a2718585be2c745f3e6233692bc6c
Bahng Ji Hoon <wpxm2003@naver.com>
2022-07-28 19:46:32 +0900

    스웨거 응답 설명 추가하기2

--- src/main/java/com/wancs/howmuchspend/dto/PostsResponseDto.java
+++ src/main/java/com/wancs/howmuchspend/dto/PostsResponseDto.java
@@ -5,15 +5,17 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
-@ApiModel(value="조회할 게시글 정보")
+@ApiModel(value="PostsResponseDto : 조회할 게시글 정보")
 @Getter
 public class PostsResponseDto {
 
     private Long id;
 
-    @ApiModelProperty(value="게시글의 제목", example = "ex) 이직을 하고싶습니다. 그런데...", required = true)
+    @ApiModelProperty(value="게시글의 제목", example = "이직을 하고싶습니다. 그런데...", required = true)
     private String title;
+    @ApiModelProperty(value="게시글의 내용", example = "코드몽키입니다 개망했쥬", required = true)
     private String content;
+    @ApiModelProperty(value="게시글의 저자", example = "김영규", required = true)
     private String author;
 
     public PostsResponseDto(Posts posts){
