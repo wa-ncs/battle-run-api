0d5d5336f2dbab5b6c564d4958b2960f8210bb3b
soobinin <isb9082@naver.com>
2022-07-29 00:33:52 +0900

    jpa practice

--- src/main/java/com/wancs/howmuchspend/HowMuchSpendApplication.java
+++ src/main/java/com/wancs/howmuchspend/HowMuchSpendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HowMuchSpendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HowMuchSpendApplication.class, args);
 	}
 
