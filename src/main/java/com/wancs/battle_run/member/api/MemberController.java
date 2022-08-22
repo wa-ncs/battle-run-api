024eb7f73a06f23df4e9fef089da08474f195566
soobinin <isb9082@naver.com>
2022-08-22 15:43:41 +0900

    디렉토리 구조에 따른 명칭 변경 (controller-> api)

--- src/main/java/com/wancs/battle_run/member/api/MemberController.java
+++ src/main/java/com/wancs/battle_run/member/api/MemberController.java
@@ -1,4 +1,4 @@
-package com.wancs.battle_run.member.controller;
+package com.wancs.battle_run.member.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
