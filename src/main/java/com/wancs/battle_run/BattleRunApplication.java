c73be19e5fdb9c1e9147173ccd332659aab76ff0
limsungju <fullmoon4860@gmail.com>
2022-09-05 16:46:00 +0900

    add new line

--- src/main/java/com/wancs/battle_run/BattleRunApplication.java
+++ src/main/java/com/wancs/battle_run/BattleRunApplication.java
@@ -2,7 +2,6 @@ package com.wancs.battle_run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = "classpath:/env/common.yml", encoding = "UTF-8")
