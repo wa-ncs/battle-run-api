085d289f61ce92c85e96a468426a6d1aa2e594fc
Bahng Ji Hoon <wpxm2003@naver.com>
2022-09-19 21:22:28 +0900

    테스트 케이스

---
+++ api/src/test/java/com/wancs/battle_run/domain/running/api/RecordApiControllerTest.java
@@ -0,0 +1,86 @@
+package com.wancs.battle_run.domain.running.api;
+
+import com.wancs.battle_run.domain.running.entity.Record;
+import com.wancs.battle_run.domain.running.dto.request.SaveRecordRequestDto;
+import com.wancs.battle_run.domain.running.dao.RecordRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.profiles.active:local")
+public class RecordApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private RecordRepository recordRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        recordRepository.deleteAll();
+    }
+
+    @Test
+    public void api로_러닝기록등록() throws Exception {
+        //given
+        Float distance = 10.5F;
+        Long runningTime = 23000L;
+        SaveRecordRequestDto requestDto = SaveRecordRequestDto.builder()
+                .distance(distance)
+                .runningTime(runningTime)
+                .userId(123L)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/records";
+
+        //when
+        ResponseEntity<Object> responseEntity = restTemplate.postForEntity(url, requestDto, Object.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        List<Record> all = recordRepository.findAll();
+        assertThat(all.get(0).getDistance()).isEqualTo(distance);
+        assertThat(all.get(0).getRunningTime()).isEqualTo(runningTime);
+    }
+
+    @Test
+    public void api로_러닝기록삭제() throws Exception {
+        //given
+        Float distance = 10.5F;
+        Long runningTime = 23000L;
+        Record savedRecord = recordRepository.save(Record.builder()
+                .distance(distance)
+                .runningTime(runningTime)
+                .userId(123L)
+                .build());
+        Long deleteId = savedRecord.getId();
+
+        String url = "http://localhost:"+port+"/api/records/"+deleteId;
+        HttpEntity<Long> requestEntity = new HttpEntity<>(deleteId);
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        List<Record> all = recordRepository.findAll();
+        assertThat(all.isEmpty());
+    }
+}
---
+++ api/src/test/java/com/wancs/battle_run/domain/running/dao/RecordRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.wancs.battle_run.domain.running.dao;
+
+import com.wancs.battle_run.domain.running.dto.request.UpdateRecordRequestDto;
+import com.wancs.battle_run.domain.running.entity.Record;
+import com.wancs.battle_run.domain.running.service.RecordService;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "spring.profiles.active:local")
+public class RecordRepositoryTest {
+
+    @Autowired
+    RecordRepository recordRepository;
+
+    @Autowired
+    RecordService recordService;
+
+    @After
+    public void cleanup(){
+        recordRepository.deleteAll();
+    }
+
+    @Test
+    public void repository_등록확인(){
+        //given
+        Float distance = 10.5F;
+        Long runningTime = 23000L;
+        Long userId = 123L;
+
+        recordRepository.save(Record.builder()
+                .distance(distance)
+                .runningTime(runningTime)
+                .userId(userId)
+                .build());
+
+        //when
+        List<Record> recordList = recordRepository.findAll();
+
+        //then
+        Record record = recordList.get(0);
+        assertThat(record.getDistance()).isEqualTo(distance);
+        assertThat(record.getRunningTime()).isEqualTo(runningTime);
+    }
+
+    @Test
+    public void service_수정확인(){
+
+        Long recordsId = recordRepository.save(Record.builder()
+                .distance(10.5F)
+                .runningTime(23000L)
+                .calorie(10)
+                .imageUrl("22323")
+                .build()).getId();
+
+        UpdateRecordRequestDto requestDto = new UpdateRecordRequestDto();
+        requestDto.setDistance(10.5F);
+        requestDto.setRunningTime(56000L);
+        requestDto.setCalorie(10);
+        requestDto.setImageUrl("22323");
+
+        recordService.update(recordsId, requestDto);
+
+        //when
+        Record records = recordService.findByRecord(recordsId);
+        assertThat("20".equals(records.getRunningTime()));
+    }
+}
